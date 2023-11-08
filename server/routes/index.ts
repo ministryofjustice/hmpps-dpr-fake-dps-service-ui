fad99a6e2cf9a6e9292e5c21280193581695b4f3
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2023-11-08 09:10:04 +0000

    [DPR2-122] API integration (#10)

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,16 +1,37 @@
-import { type RequestHandler, Router } from 'express'
-
+import { NextFunction, Request, type RequestHandler, Response, Router } from 'express'
+import ReportListUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/report-list/utils'
 import asyncMiddleware from '../middleware/asyncMiddleware'
-import type { Services } from '../services'
+import config from '../config'
+
+const defaultTokenProvider = (request: Request, response: Response, next: NextFunction): string => {
+  if (response.locals.user && response.locals.user.token) {
+    return response.locals.user.token
+  }
+  next('Could not find user token in response.locals.user.token')
+  return null
+}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function routes(service: Services): Router {
+export default function routes(): Router {
   const router = Router()
   const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
-  get('/', (req, res, next) => {
+  get('/', (req, res) => {
     res.render('pages/index')
   })
+
+  get(
+    '/list',
+    ReportListUtils.createReportListRequestHandler({
+      title: 'Test app',
+      definitionName: 'fake-preferences',
+      variantName: 'all',
+      apiUrl: config.apis.report.url,
+      apiTimeout: 8000,
+      layoutTemplate: 'partials/layout.njk',
+      tokenProvider: defaultTokenProvider,
+    }),
+  )
 
   return router
 }
